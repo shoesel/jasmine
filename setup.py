8571dee979cd28094b82ec206789047ea73a2f19
Greg Cobb and JR Boyens <pair+gcobb+jboyens@pivotallabs.com>
2013-08-09 15:01:47 -0700

    Bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages, os
 
 setup(
     name="jasmine-core",
-    version="1.3.1",
+    version="2.0.0",
     url="http://pivotal.github.io/jasmine/",
     author="Pivotal Labs",
     author_email="jasmine-js@googlegroups.com",
